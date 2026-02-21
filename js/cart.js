957d52ec8bfc31eb2b768be1d63578e9e62a5bfd
dianiluz <dianiluz05@gmail.com>
2026-02-21 15:22:46 -0500

    Update cart.js

--- js/cart.js
+++ js/cart.js
@@ -1,25 +1,4 @@
 let carrito = JSON.parse(localStorage.getItem("carrito") || "[]");
-let favoritos = JSON.parse(localStorage.getItem("favoritos") || "[]");
-
-function actualizarContadores(){
-
-  const cartEl = document.getElementById("cartCount");
-  const favEl  = document.getElementById("favCount");
-
-  if(cartEl){
-    cartEl.textContent = carrito.reduce((a,b)=>a+b.cantidad,0);
-  }
-
-  if(favEl){
-    favEl.textContent = favoritos.length;
-  }
-}
-
-function guardar(){
-  localStorage.setItem("carrito", JSON.stringify(carrito));
-  localStorage.setItem("favoritos", JSON.stringify(favoritos));
-  actualizarContadores();
-}
 
 function agregarCarrito(prod){
   let existe = carrito.find(p=>p.ref===prod.ref && p.talla===prod.talla);
@@ -27,22 +6,5 @@ function agregarCarrito(prod){
   if(existe) existe.cantidad += prod.cantidad;
   else carrito.push(prod);
 
-  guardar();
+  localStorage.setItem("carrito", JSON.stringify(carrito));
 }
-
-function agregarFavorito(prod){
-  if(!favoritos.find(p=>p.ref===prod.ref)){
-    favoritos.push(prod);
-    guardar();
-  }
-}
-
-function abrirCarrito(){
-  alert("Carrito conectado próximamente a WhatsApp");
-}
-
-function abrirFavoritos(){
-  alert("Favoritos próximamente");
-}
-
-document.addEventListener("DOMContentLoaded", actualizarContadores);
