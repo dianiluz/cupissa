5f21af23cf23d7a22d15b49bf1ddad607e4b9254
dianiluz <dianiluz05@gmail.com>
2026-02-21 16:28:36 -0500

    Update search.js

--- js/search.js
+++ js/search.js
@@ -1,14 +1,15 @@
-let productosGlobalBusqueda = [];
+let productosBusqueda = [];
 
-async function cargarProductosBusqueda(){
+async function cargarBusqueda(){
 
   const res = await fetch("https://docs.google.com/spreadsheets/d/e/2PACX-1vQB2HWydVva17mDTdLcYgY409q5DcJHg3PumZLypAgLiwWs6s8ptH_kC_qjuhZv7W010xobmyFl2d7y/pub?output=csv");
   const texto = await res.text();
 
   const filas = texto.split("\n").slice(1);
 
-  productosGlobalBusqueda = filas.map(f=>{
+  productosBusqueda = filas.map(f=>{
     const c = f.split(/,(?=(?:(?:[^"]*"){2})*[^"]*$)/);
+
     return {
       nombre:c[1]?.replace(/"/g,"").trim(),
       mundo:c[3]?.replace(/"/g,"").trim(),
@@ -20,13 +21,14 @@ async function cargarProductosBusqueda(){
       palabras:c[12]?.replace(/"/g,"").trim(),
       activo:c[13]?.replace(/"/g,"").trim()
     };
+
   }).filter(p=>p.activo?.toLowerCase().includes("si"));
 
 }
 
 document.addEventListener("DOMContentLoaded", ()=>{
 
-  cargarProductosBusqueda();
+  cargarBusqueda();
 
   const input = document.getElementById("buscadorGlobal");
   if(!input) return;
@@ -34,17 +36,18 @@ document.addEventListener("DOMContentLoaded", ()=>{
   input.addEventListener("input", e=>{
 
     const texto = e.target.value.toLowerCase();
-
     if(texto.length < 2) return;
 
-    const resultados = productosGlobalBusqueda.filter(p=>
+    const resultados = productosBusqueda.filter(p=>
       p.nombre?.toLowerCase().includes(texto) ||
       p.tematica?.toLowerCase().includes(texto) ||
       p.ocasion?.toLowerCase().includes(texto) ||
       p.palabras?.toLowerCase().includes(texto)
     );
 
-    console.log("Resultados:", resultados);
+    // Redirige automáticamente al catálogo con query
+    window.location.href = `/catalogo/?buscar=${texto}`;
+
   });
 
 });
